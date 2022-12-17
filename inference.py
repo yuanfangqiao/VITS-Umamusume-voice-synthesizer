c6070dbcda03c88d8b372e72a8078140ee60207c
ElderFrog <Plachta@users.noreply.huggingface.co>
2022-12-17 15:39:43 +0000

    Upload 66 files

---
+++ inference.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import IPython.display as ipd
+
+import os
+import json
+import math
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+
+import commons
+import utils
+from data_utils import TextAudioLoader, TextAudioCollate, TextAudioSpeakerLoader, TextAudioSpeakerCollate
+from models import SynthesizerTrn
+from text.symbols import symbols
+from text import text_to_sequence
+
+from scipy.io.wavfile import write
+
+
+def get_text(text, hps):
+    text_norm = text_to_sequence(text, hps.data.text_cleaners)
+    if hps.data.add_blank:
+        text_norm = commons.intersperse(text_norm, 0)
+    text_norm = torch.LongTensor(text_norm)
+    return text_norm
+
+
+hps = utils.get_hparams_from_file("./configs/yuzu.json")
+
+net_g = SynthesizerTrn(
+    len(symbols),
+    hps.data.filter_length // 2 + 1,
+    hps.train.segment_size // hps.data.hop_length,
+    n_speakers=hps.data.n_speakers,
+    **hps.model).cuda()
+_ = net_g.eval()
+
+_ = utils.load_checkpoint("pretrained_models/yuzu.pth", net_g, None)
