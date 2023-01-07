12a0f395266d454e55cecd49d88a59f5aec0343a
ElderFrog <Plachta@users.noreply.huggingface.co>
2023-01-07 04:50:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -47,11 +47,8 @@ If your input language is not Japanese, it will be translated to Japanese by Goo
 article = """
 
 """
-duration_slider = gr.Slider(minimum=0.1, maximum=5, value=1, step=0.1, label='时长 Duration', interactive=True)
-noise_scale_slider = gr.Slider(minimum=0.1, maximum=5, value=0.667, step=0.001, label='噪声比例 noise_scale', interactive=True),
-noise_scale_w_slider = gr.Slider(minimum=0.1, maximum=5, value=0.8, step=0.1, label='噪声偏差 noise_scale_w', interactive=True),
 
-def infer(text, character, language, duration, noise_scale, noise_scale_w):
+def infer(text, character, language):
     if language == '日本語':
         pass
     elif language == '简体中文':
@@ -64,7 +61,7 @@ def infer(text, character, language, duration, noise_scale, noise_scale_w):
         x_tst = stn_tst.unsqueeze(0)
         x_tst_lengths = torch.LongTensor([stn_tst.size(0)])
         sid = torch.LongTensor([char_id])
-        audio = net_g.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=noise_scale, noise_scale_w=noise_scale_w, length_scale=duration)[0][0,0].data.cpu().float().numpy()
+        audio = net_g.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=0.667, noise_scale_w=0.8, length_scale=1)[0][0,0].data.cpu().float().numpy()
     return (text,(22050, audio))
 
 # We instantiate the Textbox class
@@ -99,5 +96,5 @@ examples = [['お疲れ様です，トレーナーさん。', '1:无声铃鹿', 
             ['授業中に出しだら，学校生活終わるですわ。', '12:目白麦昆','日本語'],
             ['お帰りなさい，お兄様！', '29:米浴','日本語'],
             ['私の処女をもらっでください！', '29:米浴','日本語']]
-gr.Interface(fn=infer, inputs=[textbox, char_dropdown, language_dropdown, duration_slider, noise_scale_slider, noise_scale_w_slider], outputs=["text","audio"],
+gr.Interface(fn=infer, inputs=[textbox, char_dropdown, language_dropdown], outputs=["text","audio"],
             title=title, description=description, article=article, examples = examples).launch()
