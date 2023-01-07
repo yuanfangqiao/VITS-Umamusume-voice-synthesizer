f5cf4cf7920a2aad815a293f9283682af8ad1dec
ElderFrog <Plachta@users.noreply.huggingface.co>
2023-01-07 21:32:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -31,8 +31,8 @@ title = "Umamusume voice synthesizer \n 赛马娘语音合成器"
 description = """
 This synthesizer is created based on VITS (https://arxiv.org/abs/2106.06103) model, trained on voice data extracted from mobile game Umamusume Pretty Derby\n
 这个合成器是基于VITS文本到语音模型，在从手游《賽馬娘：Pretty Derby》解包的语音数据上训练得到。\n
-Link to introduction video: https://www.bilibili.com/video/BV1T84y1e7p5/?vd_source=6d5c00c796eff1cbbe25f1ae722c2f9f#reply607277701\n
-模型介绍视频链接：https://www.bilibili.com/video/BV1T84y1e7p5/?vd_source=6d5c00c796eff1cbbe25f1ae722c2f9f#reply607277701\n
+Link to introduction video  模型介绍视频链接：\n
+https://www.bilibili.com/video/BV1T84y1e7p5/?vd_source=6d5c00c796eff1cbbe25f1ae722c2f9f#reply607277701\n
 Due to some unknown reason, VITS inference on CPU results in accumulative memory leakage, resulting in Runtime error：Memory limit exceeded.\n
 In case of space crash, you may duplicate this space to run it privately and without any queue.\n
 由于未知原因，VITS模型在CPU上执行推理时会有逐步累积的内存泄漏，最终导致空间报错Runtime error：Memory limit exceeded，目前正在排查。\n
